92dcac4ba0bfeccde5ba315104da463bab7148d2
Samuel Souza <smksouza.dev@gmail.com>
2023-11-18 11:55:46 -0300

    created user controller

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,23 @@
+import { prisma } from '@/lib/prisma'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { hash } from 'bcryptjs'
+import { RegisterUseCase } from '@/use-cases/register'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    await RegisterUseCase({ name, email, password })
+  } catch (error) {
+    return reply.status(409).send()
+  }
+
+  return reply.status(201).send()
+}
