b9bb35fdb98aad81d2fb744966ab505af1293327
Samuel Souza <smksouza.dev@gmail.com>
2023-11-20 15:52:03 -0300

    handler global errors

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -24,7 +24,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
         message: error.message,
       })
     }
-    return reply.status(500).send()
+    throw error
   }
 
   return reply.status(201).send()
