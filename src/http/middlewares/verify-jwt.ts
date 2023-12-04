d3b8192d753bee576d3d218bf3fe10dcbf820c59
Samuel Souza <smksouza.dev@gmail.com>
2023-12-04 09:06:17 -0300

    controller get profile created

---
+++ src/http/middlewares/verify-jwt.ts
@@ -0,0 +1,8 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+export async function VerifyJWT(request: FastifyRequest, reply: FastifyReply) {
+  try {
+    await request.jwtVerify()
+  } catch (error) {
+    return reply.status(401).send({ message: 'Unauthorized.' })
+  }
+}
