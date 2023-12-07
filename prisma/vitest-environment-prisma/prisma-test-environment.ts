36dafbb8c1916657c7b6e57f7388f73af388e8a6
Samuel Souza <smksouza.dev@gmail.com>
2023-12-07 20:48:35 -0300

    não consigo fazer o setup execultar antes do register.spec.ts

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,30 +1,14 @@
-import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
-
-// "postgresql://docker:docker@localhost:5432/apisolid?schema=public"
-
-function generateDatabaseURL(schema: string) {
-  if (!process.env.DATABASE_URL) {
-    throw new Error('Please provide a DATABASE_URL ENVIRONMENT VARIABLE.')
-  }
-
-  const url = new URL(process.env.DATABASE_URL)
-
-  url.searchParams.set('schema', schema)
-
-  return url.toString()
-}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
 
   async setup() {
-    const schema = randomUUID()
-    console.log(generateDatabaseURL(schema))
+    console.log('CHEGUEI AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII')
     return {
       teardown() {
-        console.log('Teardown')
+        console.log('TEARDOWNNNNNNNNNNNNNNNNN')
       },
     }
   },
