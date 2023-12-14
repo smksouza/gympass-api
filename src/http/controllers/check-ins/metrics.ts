89532d4c8604671208084bfeac6843716f905171
Samuel Souza <smksouza.dev@gmail.com>
2023-12-14 18:46:28 -0300

    all routes by check-ins created

--- src/http/controllers/check-ins/metrics.ts
+++ src/http/controllers/check-ins/metrics.ts
@@ -1,7 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { makeGetUserMetricsUseCase } from '@/use-cases/factories/make-get-user-metrics-use-case'
 
-export async function history(request: FastifyRequest, reply: FastifyReply) {
+export async function metrics(request: FastifyRequest, reply: FastifyReply) {
   const getUserMetricsUseCase = makeGetUserMetricsUseCase()
 
   const { checkInsCount } = await getUserMetricsUseCase.execute({
--- src/http/controllers/check-ins/validate.ts
+++ src/http/controllers/check-ins/validate.ts
@@ -1,6 +1,5 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
-import { makeCheckInUseCase } from '@/use-cases/factories/make-check-in-use-case'
 import { makeValidateCheckInUseCase } from '@/use-cases/factories/make-validate-check-in-use-case'
 
 export async function validate(request: FastifyRequest, reply: FastifyReply) {
