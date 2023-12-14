aa43d7f49e374ae49f3b53798cf72bde5bf4a723
Samuel Souza <smksouza.dev@gmail.com>
2023-12-14 18:44:28 -0300

    created controller validate checkIn

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -1,10 +1,13 @@
 import { FastifyInstance } from 'fastify'
 
 import { VerifyJWT } from '../../middlewares/verify-jwt'
+
 import { create } from './create'
+import { validate } from './validate'
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', VerifyJWT)
 
   app.post('/gyms/:gymId/check-ins', create)
+  app.patch('/check-ins/:checkInId/validate', validate)
 }
